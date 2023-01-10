0166ded3bda7da0dd8f6eee8119399e455bea3c2
Wellington Araújo <wellingtonaraujo777@gmail.com>
2023-01-10 16:06:07 -0300

    Ajuste de notação à entidade Paciente e adicionando listagem de Médicos

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -31,6 +31,7 @@ public class Paciente {
         this.email = dados.email();
         this.telefone = dados.telefone();
         this.cpf = dados.cpf();
+        this.endereco = new Endereco(dados.endereco());
     }
 
 }
