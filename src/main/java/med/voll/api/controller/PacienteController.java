bd0df60f8e1f78c3e083e51b5862634905bef690
Wellington Araújo <wellingtonaraujo777@gmail.com>
2023-01-10 15:12:21 -0300

    Adicionando persistência JPA e validação com Bean Validation para a entidade Paciente

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
-import med.voll.api.controller.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("pacientes")
 public class PacienteController {
 
+    @Autowired
+    private PacienteRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroPaciente dados){
-        System.out.println("dados recebidos: " + dados);
+        repository.save(new Paciente(dados));
     }
 }
--- src/main/java/med/voll/api/controller/paciente/DadosCadastroPaciente.java
+++
@@ -1,6 +0,0 @@
-package med.voll.api.controller.paciente;
-
-import med.voll.api.endereco.DadosEndereco;
-
-public record DadosCadastroPaciente(String nome, String email, String telefone, String cpf, DadosEndereco endereco) {
-}
