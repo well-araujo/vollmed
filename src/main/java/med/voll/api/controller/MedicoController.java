fca03fc5ace65dc9cd339f7ebaea37b35cd8fdaa
Wellington Araújo <wellingtonaraujo777@gmail.com>
2023-01-12 22:47:38 -0300

    Adicionando o recurso de exclusão para a entidade Médico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -37,4 +37,10 @@ public class MedicoController {
         medico.atualizarInformacoes(dados);
     }
 
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
 }
